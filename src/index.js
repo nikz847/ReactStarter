767c97ae5a09e1f596851918afec73dd13c6e1b5
Nikhil Parulekar <nikhilp@qti.qualcomm.com>
2018-04-25 23:17:03 -0700

    committing first changes

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React, {Component} from "react"
+import ReactDOM from "react-dom"
+import YTSearch from "youtube-api-search"
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+const API_KEY = "AIzaSyDjxDvwtXMjsvyY_zzWcRFHcm4zJdr9K6o";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []}
+
+    YTSearch({key: API_KEY, term: "black metal"},(videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+    <div>
+      <SearchBar />
+      <VideoList videos={this.state.videos} />
+    </div>
+  );
+}
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"))
